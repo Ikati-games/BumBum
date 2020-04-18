799acdcccaad26ff2d667463821b04769bf9f864
bogotolec <i.makarenko@innopolis.university>
2020-04-18 15:25:22 +0300

    Added trap sprites

--- TiledFiles/TileCollection.tsx
+++ TiledFiles/TileCollection.tsx
@@ -1,9 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="TileCollection" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+<tileset version="1.2" tiledversion="1.3.4" name="TileCollection" tilewidth="16" tileheight="16" tilecount="22" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="16" height="16" source="../sprites/background/bg_tile_1_1.png"/>
- </tile>
  <tile id="1">
   <image width="16" height="16" source="../sprites/wall/wall.png"/>
  </tile>
@@ -12,5 +9,62 @@
  </tile>
  <tile id="4">
   <image width="16" height="16" source="../sprites/bum/bum.gif"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_5.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_6.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_7.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_8.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_9.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_10.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_11.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_12.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_13.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_14.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_15.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_16.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../sprites/background/bg_tile_1_1.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../sprites/trap/trap_closed.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../sprites/trap/trap_horizontal.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../sprites/trap/trap_vertical.png"/>
  </tile>
 </tileset>
