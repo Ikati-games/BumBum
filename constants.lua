d82ced2810d5031ca136e27e523bf4cf92009272
Sergey Semushin <s.semushin@innopolis.university>
2020-04-18 04:06:26 +0300

    Added good level selection menu

--- constants.lua
+++ constants.lua
@@ -5,6 +5,7 @@ T.menuButtonHeight = display.contentHeight/10
 T.menuButtonWidth = display.contentWidth*9/10
 T.menuButtonInterval = 20
 T.settingsButtonWidth = T.menuButtonHeight*2
+T.levelSelectInterval = T.menuButtonHeight/2
 
 T.minSwipeSize = 50
 
--- scenes/level_select.lua
+++ scenes/level_select.lua
@@ -1,31 +1,44 @@
-local scene_getter = require("scenes.menu_scene_getter")
-
-
-
 function gotoLevel(minigameId, levelId)
-	composer.gotoScene("scenes.level", {
-		params = {
-			minigameId = minigameId,
-			levelId = levelId
-		}
-	})
 end
 
 
 
-function getLevels(params)
-	local levels = {}
-	for i = 1, C.levelsAmount[params.minigameId] do
-		local levelId = i;
-		table.insert(levels, {
-			img = "sprites/button/button_sound.png",
-			imgPressed = "sprites/button/button_music.png",
-			func = function() gotoLevel(params.minigameId, levelId) end,
+local scene = composer.newScene()
+scene:addEventListener("create", function(event)
+	local background = display.newImageRect(scene.view, C.menuBackgroundImage, display.contentWidth, display.contentHeight)
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+
+	local xIndex = -1
+	local y = C.levelSelectInterval + C.menuButtonHeight / 2
+
+	for i = 1, C.levelsAmount[event.params.minigameId] do
+		local levelId = i
+
+		print(xIndex, y)
+		button = widget.newButton({
+			x = display.contentCenterX + xIndex * (C.levelSelectInterval + C.menuButtonHeight), 
+			y = y,
+			width = C.menuButtonHeight,
+			height = C.menuButtonHeight,
+			defaultFile = "sprites/button/button_"..i..".png",
+			overFile = "sprites/button/button_"..i.."_pressed.png",
+			onRelease = function ()
+				composer.gotoScene("scenes.level", {
+					params = {
+						minigameId = event.params.minigameId,
+						levelId = levelId
+					}
+				})
+			end
 		})
+		scene.view:insert(button)
+
+		xIndex = xIndex + 1
+		if (xIndex > 1) then
+			xIndex = -1
+			y = y + C.levelSelectInterval + C.menuButtonHeight
+		end
 	end
-	return levels
-end
-
-
-
-return scene_getter(getLevels)
+end)
+return scene
