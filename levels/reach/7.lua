f3118dbbcd23c0a6fbc6b43801036549bc6df2d5
bogotolec <i.makarenko@innopolis.university>
2020-04-22 12:32:51 +0300

    New level

---
+++ levels/reach/7.lua
@@ -0,0 +1,355 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.3.4",
+  orientation = "orthogonal",
+  renderorder = "left-up",
+  width = 12,
+  height = 14,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 4,
+  nextobjectid = 5,
+  properties = {},
+  tilesets = {
+    {
+      name = "TileCollection",
+      firstgid = 1,
+      filename = "../../TiledFiles/TileCollection.tsx",
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 1,
+        height = 1
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 26,
+      tiles = {
+        {
+          id = 1,
+          image = "../../sprites/wall/wall.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 2,
+          image = "../../sprites/trashcan/trashcan.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 5,
+          image = "../../sprites/background/bg_tile_1_2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 6,
+          image = "../../sprites/background/bg_tile_1_3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 7,
+          image = "../../sprites/background/bg_tile_1_4.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 8,
+          image = "../../sprites/background/bg_tile_1_5.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 9,
+          image = "../../sprites/background/bg_tile_1_6.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 10,
+          image = "../../sprites/background/bg_tile_1_7.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 11,
+          image = "../../sprites/background/bg_tile_1_8.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 12,
+          image = "../../sprites/background/bg_tile_1_9.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 13,
+          image = "../../sprites/background/bg_tile_1_10.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 14,
+          image = "../../sprites/background/bg_tile_1_11.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 15,
+          image = "../../sprites/background/bg_tile_1_12.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 16,
+          image = "../../sprites/background/bg_tile_1_13.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 17,
+          image = "../../sprites/background/bg_tile_1_14.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 18,
+          image = "../../sprites/background/bg_tile_1_15.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 19,
+          image = "../../sprites/background/bg_tile_1_16.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 20,
+          image = "../../sprites/background/bg_tile_1_1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 21,
+          image = "../../sprites/trap/trap_closed.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 22,
+          image = "../../sprites/trap/trap_horizontal.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 23,
+          image = "../../sprites/trap/trap_vertical.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 24,
+          image = "../../sprites/trap/trap_both.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 29,
+          image = "../../sprites/change/change_f1.png",
+          width = 16,
+          height = 16,
+          animation = {
+            {
+              tileid = 29,
+              duration = 250
+            },
+            {
+              tileid = 30,
+              duration = 250
+            }
+          }
+        },
+        {
+          id = 30,
+          image = "../../sprites/change/change_f2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 32,
+          image = "../../sprites/bum/bum_f1.png",
+          width = 16,
+          height = 16,
+          animation = {
+            {
+              tileid = 32,
+              duration = 250
+            },
+            {
+              tileid = 33,
+              duration = 250
+            }
+          }
+        },
+        {
+          id = 33,
+          image = "../../sprites/bum/bum_f2.png",
+          width = 16,
+          height = 16
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 1,
+      name = "floor",
+      x = 0,
+      y = 0,
+      width = 12,
+      height = 14,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        16, 21, 19, 14, 9, 18, 14, 19, 20, 15, 12, 15,
+        14, 19, 15, 19, 7, 21, 16, 18, 9, 7, 17, 15,
+        6, 19, 6, 15, 6, 15, 19, 19, 6, 8, 7, 12,
+        10, 17, 13, 12, 17, 10, 9, 6, 12, 17, 14, 6,
+        21, 15, 6, 10, 9, 8, 6, 18, 15, 9, 9, 18,
+        8, 17, 8, 6, 14, 18, 8, 14, 11, 14, 9, 15,
+        8, 11, 17, 7, 15, 12, 10, 7, 8, 12, 10, 6,
+        13, 19, 7, 13, 9, 21, 18, 11, 20, 6, 7, 18,
+        9, 7, 10, 14, 7, 12, 8, 14, 6, 12, 13, 16,
+        6, 15, 15, 20, 21, 21, 6, 12, 13, 20, 10, 17,
+        19, 19, 9, 7, 15, 8, 17, 18, 11, 7, 9, 9,
+        8, 13, 6, 9, 10, 21, 9, 6, 17, 20, 10, 16,
+        7, 6, 12, 10, 11, 14, 11, 20, 8, 9, 7, 11,
+        11, 11, 11, 15, 19, 11, 10, 15, 17, 13, 16, 21
+      }
+    },
+    {
+      type = "tilelayer",
+      id = 2,
+      name = "walls",
+      x = 0,
+      y = 0,
+      width = 12,
+      height = 14,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0,
+        0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0,
+        2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+        0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      id = 3,
+      name = "objects",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "trap1",
+          type = "trap",
+          shape = "rectangle",
+          x = 96,
+          y = 64,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 24,
+          visible = true,
+          properties = {
+            ["isHorizontalAllowed"] = false,
+            ["isVerticalAllowed"] = true
+          }
+        },
+        {
+          id = 2,
+          name = "trap2",
+          type = "trap",
+          shape = "rectangle",
+          x = 127,
+          y = 192,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 23,
+          visible = true,
+          properties = {
+            ["isHorizontalAllowed"] = true,
+            ["isVerticalAllowed"] = false
+          }
+        },
+        {
+          id = 3,
+          name = "player",
+          type = "",
+          shape = "rectangle",
+          x = 112,
+          y = 80,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 33,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 4,
+          name = "finish",
+          type = "",
+          shape = "rectangle",
+          x = 112,
+          y = 176,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 3,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
