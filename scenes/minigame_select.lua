f9a784e8643d40a307f58623646e9b76c855c2a7
Sergey Semushin <s.semushin@innopolis.university>
2020-04-27 08:24:37 +0300

    Skipping minigame selection if there is only one minigame

--- scenes/minigame_select.lua
+++ scenes/minigame_select.lua
@@ -28,5 +28,25 @@ end
 
 
 local scene = scene_getter(getMinigames)
+scene:addEventListener("show", function(event)
+	-- if there is only one minigame, skip this scene
+	
+	local theOnlyMinigame = nil
+	for minigame in pairs(C.levelsAmount) do
+		if theOnlyMinigame ~= nil then return end
+		theOnlyMinigame = minigame
+	end
+
+	local previousScene = composer.getSceneName("previous")
+	if previousScene == "scenes.level_select" then
+		composer.gotoScene("scenes.main_menu")
+	elseif previousScene == "scenes.main_menu" then
+		composer.gotoScene("scenes.level_select", {
+			params = {
+				minigameId = theOnlyMinigame
+			}
+		})
+	end
+end)
 scene.previousScene = "scenes.main_menu"
 return scene
