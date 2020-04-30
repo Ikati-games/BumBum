f70747ad1865f4a5292182c03d8f9ffeb451d034
bogotolec <i.makarenko@innopolis.university>
2020-04-30 11:20:32 +0300

    Additional files

--- TiledFiles/reach_tutorial.tsx
+++ TiledFiles/reach_tutorial.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="reach_tutorial" tilewidth="64" tileheight="64" tilecount="16" columns="16">
  <image source="../sprites/tutorial/reach_tutorial.png" width="1024" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="14" duration="50"/>
+   <frame tileid="15" duration="50"/>
+  </animation>
+ </tile>
 </tileset>
