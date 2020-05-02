66e01e408b44efdee9ae30f1b6e428a4a1fd0ef5
bogotolec <i.makarenko@innopolis.university>
2020-05-02 13:30:43 +0300

    Added level 25

--- constants.lua
+++ constants.lua
@@ -24,11 +24,11 @@ T.confirmationScreenHeight = 34*T.pixelSize
 T.minSwipeSize = 25
 
 T.levelsAmount = {
-	reach = 24,
+	reach = 25,
 }
 
 T.collectibles = {
-	reach = {5, 9, 13, 18, 19}
+	reach = {5, 9, 13, 18, 19, 25}
 }
 T.collectibleCollectedImage = "sprites/change/change_f1.png"
 T.collectibleUncollectedImage = "sprites/change/change_uncollected.png"
